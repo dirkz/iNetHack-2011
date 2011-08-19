7c8c177248a8de4258b2025d5083958d4cb6ad74
Dirk Zimmermann <me+github@dirkz.com>
2011-08-19 07:48:12 +0200

    clear-text error message for glCheckError

--- win/iOS/DLog/DLog.h
+++ win/iOS/DLog/DLog.h
@@ -27,12 +27,30 @@
 #endif // DLOG
 #endif // __OBJC__
 
+#define GL_NO_ERROR                       0
+#define GL_INVALID_ENUM                   0x0500
+#define GL_INVALID_VALUE                  0x0501
+#define GL_INVALID_OPERATION              0x0502
+#define GL_STACK_OVERFLOW                 0x0503
+#define GL_STACK_UNDERFLOW                0x0504
+#define GL_OUT_OF_MEMORY                  0x0505
+
 // Catch run-time GL errors
 #if defined(DEBUG) || defined(DLOG)
 #define glCheckError() { \
 GLenum err = glGetError(); \
 if (err != GL_NO_ERROR) { \
-fprintf(stderr, "glCheckError: x%04x caught at %s:%u\n", err, __FILE__, __LINE__); \
+char *msg; \
+switch (err) { \
+case 0x500: msg = "GL_INVALID_ENUM"; break; \
+case 0x501: msg = "GL_INVALID_VALUE"; break; \
+case 0x502: msg = "GL_INVALID_OPERATION"; break; \
+case 0x503: msg = "GL_STACK_OVERFLOW"; break; \
+case 0x504: msg = "GL_STACK_UNDERFLOW"; break; \
+case 0x505: msg = "GL_OUT_OF_MEMORY"; break; \
+default: msg = "Unknown"; break; \
+} \
+fprintf(stderr, "glCheckError: 0x%04x (%s) caught at %s:%u\n", err, msg, __FILE__, __LINE__); \
 assert(0); \
 } \
 }
